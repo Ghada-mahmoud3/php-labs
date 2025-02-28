8bef3c5dfd00782cbe0e3a0af8013ba1e56d638f
Ghada-mahmoud3 <ghadaabdelhady3@gmail.com>
2025-02-28 16:03:43 +0200

    ..

--- lab4/templates/footer.php
+++
@@ -1,4 +0,0 @@
-        <!-- copyright -->
-        <div class="colorlibcopy-agile">
-            <p>© 2025 All rights reserved to Dodo</p>
-        </div>
--- lab4/templates/head.php
+++
@@ -1,9 +0,0 @@
-<head>
-    <title>Edit User</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <!-- Custom Theme files -->
-    <link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
-    <!-- //Custom Theme files -->
-    <link href="//fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,700,700i" rel="stylesheet">
-</head>
